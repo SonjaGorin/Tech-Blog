dab9ccc194728af14ef892b0a07fcce9eaa3357f
Sonja <sonjaljatkovic@gmail.com>
2023-11-12 14:23:14 -0500

    added signup.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,14 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector("#username-input").value.trim();
-    const password = document.querySelector("#password-input").value.trim();
+    const username = document.querySelector("#username-login-input").value.trim();
+    const password = document.querySelector("#password-login-input").value.trim();
 
     if (username && password) {
         const response = await fetch("", {
             method: "POST",
             body: JSON.stringify({ username, password}),
-            headers: { 'Content-Type': 'application/json' },
+            headers: { "Content-Type": "application/json" },
         });
 
         if (response.ok) {
---
+++ public/js/sign-up.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector("#username-signup-input").value.trim();
+    const password = document.querySelector("#password-signup-input").value.trim();
+
+    if (username && password) {
+        const response = await fetch("", {
+            method: "POST",
+            body: JSON.stringify({ username, password}),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert("Failed to sign up.");
+        }
+    }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
