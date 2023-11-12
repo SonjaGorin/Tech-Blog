d454b6cc459b1d2ee4afabc7924889cedc72622a
Sonja <sonjaljatkovic@gmail.com>
2023-11-12 17:09:49 -0500

    added sign up and log out post methods

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         const dbUserData = await User.findOne({
             where: {
@@ -12,7 +12,7 @@ router.post('/login', async (req, res) => {
         if (!dbUserData) {
             res
                 .status(400)
-                .json({ message: "Incorrect email or password. Please try again!" });
+                .json({ message: "Incorrect username or password. Please try again!" });
             return;
         }
     
@@ -21,7 +21,7 @@ router.post('/login', async (req, res) => {
         if (!validPassword) {
             res
                 .status(400)
-                .json({ message: "Incorrect email or password. Please try again!" });
+                .json({ message: "Incorrect username or password. Please try again!" });
             return;
         }
 
@@ -38,3 +38,32 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post("/", async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+    
+        req.session.save(() => {
+            req.session.loggedIn = true;
+    
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
