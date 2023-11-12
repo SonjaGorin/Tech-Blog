97a9f345eb07d8104d1925d6b2e5171ebb6b4224
Sonja <sonjaljatkovic@gmail.com>
2023-11-12 14:31:30 -0500

    added logout.js

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const logout = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+        document.location.replace("/");
+      } else {
+        alert("Failed to log out.");
+      }
+};
+
+document
+    .querySelector("#logout")
+    .addEventListener("click", logout);
