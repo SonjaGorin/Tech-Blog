6833ab55c145669da0bae4a569e5cdd229b17b16
Sonja <sonjaljatkovic@gmail.com>
2023-11-14 19:39:22 -0500

    added post, put and delete routes for posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -44,3 +44,48 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.post("/", async (req, res) => {
+    try {
+        const newPost = await Post.create(req.body);
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedPost = await Post.update({
+            title: req.body.title,
+            content: req.body.content
+    },
+    {
+        where: {
+            id: req.params.id,
+        },
+    });
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const dbPostData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found with that id!" });
+            return;
+        };
+        res.status(200).json(dbPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
