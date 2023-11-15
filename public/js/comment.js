e2e50ba6342c7e22fd18c77e618df7be89a90b14
Sonja <sonjaljatkovic@gmail.com>
2023-11-15 17:49:30 -0500

    added comment.js

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const content = document.querySelector("#comment-content-input").value.trim();
+    const post_id = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1
+    ];
+
+    if (content) {
+        const response = await fetch("", {
+            method: "POST",
+            body: JSON.stringify({ post_id, content }),
+            headers: {"Content-Type": "application/json"}
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert("Failed to post a comment.");
+        }
+    }
+};
+
+document
+    .querySelector(".comment-form")
+    .addEventListener("submit", commentFormHandler);
