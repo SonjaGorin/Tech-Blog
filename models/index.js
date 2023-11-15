0237e1af3cd36736a80d39d3f0dd8da69566c1c8
Sonja <sonjaljatkovic@gmail.com>
2023-11-15 17:25:57 -0500

    added associations to models/index.js

--- models/index.js
+++ models/index.js
@@ -11,4 +11,24 @@ Post.belongsTo(User, {
     onDelete: "cascade"
 });
 
-module.exports = { User, Post }
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "cascade"
+})
+
+module.exports = { User, Post, Comment }
