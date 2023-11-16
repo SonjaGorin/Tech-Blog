d1b031746a392cc70b07c6b057501446e235f579
Sonja <sonjaljatkovic@gmail.com>
2023-11-16 14:05:27 -0500

    fix the issues

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -74,7 +74,11 @@ router.get("/:id", withAuth, async (req, res) => {
 
 router.post("/", withAuth, async (req, res) => {
     try {
-        const newPost = await Post.create(req.body);
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
         res.status(200).json(newPost);
     } catch (err) {
         res.status(400).json(err);
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
@@ -13,8 +15,14 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: "Super secret secret",
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+  },
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
