340e3f9ab553dc0720f80bb539f9543521a7e1f1
Sonja <sonjaljatkovic@gmail.com>
2023-11-16 11:10:33 -0500

    added delete and edit handlebars and delete-post.js

---
+++ public/js/delete-post.js
@@ -0,0 +1,27 @@
+const deletePostFormHandler = async (event) => {
+    event.preventDefault();
+
+    const postId = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1];
+      
+    const response = await fetch(`/api/posts/${postId}`, {
+        method: "DELETE",
+        body: JSON.stringify({
+            post_id: postId
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+      
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        alert(response.statusText);
+    }
+      
+}
+
+document
+    .querySelector(".delete-post-button")
+    .addEventListener("click", deletePostFormHandler);
